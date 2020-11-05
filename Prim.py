34c6f996c6fedc5a03109596b571fda356201906
Gwilherm LESNE <gwilherm.lesne@telecom-paristech.fr>
2020-11-05 12:24:12 +0100

    Cleanest version of code with results

--- Prim.py
+++ Prim.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras import backend as K
 import scipy.optimize
+import scipy.signal
 
 print('Imports done')
 
@@ -20,33 +21,33 @@ model = VGG19(include_top=True, pooling="avg", classes=1000)
 
 model.summary()
 
+layers_list=[1,4,7,12,17] #indexes of the layers we want to compute the gram matrix
+layers_list_bis=["block1_conv1","block2_conv1","block3_conv1","block4_conv1","block5_conv1"]
+
+layer=[[1],[1,4],[1,4,7],[1,4,7,12],[1,4,7,12,17]]
+layer2=[[1],[4],[7],[12],[17]]
+layer3=[layers_list]
+
 #%% Testing the CNN
+def Test_CNN(img_path):    
+    img = image.load_img(img_path, target_size=(224, 224),interpolation="bicubic")
+    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    #show the image
+    plt.imshow(img)
+    plt.plot()    
+    #predict the class of the image
+    prediction = model.predict(img_tensor)
+    pred = np.argmax(prediction, axis = 1)
+    print("class number:", pred[0])    
+    #convert index of the prediction into a readable label
+    with open("imagenet_labels.txt") as file_in:
+        labels = []
+        for line in file_in:
+            labels.append(line)
+    print(labels[pred[0]+1])
 
-img_path = "tiger.jpg"
-
-img = image.load_img(img_path, target_size=(224, 224),interpolation="bicubic")
-img_tensor = image.img_to_array(img)                    # (height, width, channels)
-img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-
-#show the image
-
-#plt.imshow(img)
-#plt.plot()
-
-#predict the class of the image
-prediction = model.predict(img_tensor)
-pred = np.argmax(prediction, axis = 1)
-print("class number:", pred[0])
-
-#convert index of the prediction into a readable label
-with open("imagenet_labels.txt") as file_in:
-    labels = []
-    for line in file_in:
-        labels.append(line)
-print(labels[pred[0]+1])
-
-#%%Gram matrix & Loss function definition
-
+#Gram matrix & Loss function definition
 def gram_Matrix(activation):
     act=activation[0]
     s = act.shape
@@ -65,37 +66,7 @@ def get_Tloss(act1,act2,layers_list):
 def texture_loss(layers_list,act1,act2):
     return get_Tloss(act1,act2,layers_list)
 
-#%% Define the source texture
-
-layers_list=[1,4,7,12,17] #indexes of the layers we want to compute the gram matrix
-layers_list_bis=["block1_conv1","block2_conv1","block3_conv1","block4_conv1","block5_conv1"]
-
-src_img_path = "texture1.jpg"  #There is 5 textures for the moment.
-
-src_img = image.load_img(src_img_path, target_size=(224, 224),interpolation="bicubic")
-src_img_tensor = image.img_to_array(src_img)                    # (height, width, channels)
-src_img_tensor = np.expand_dims(src_img_tensor, axis=0)
-
-outputs = [layer.output for layer in model.layers]
-active_func = K.function([model.input], [outputs])
-src_act = active_func(src_img_tensor)
-
-#create white gaussian noise
-x0 = np.random.normal(127,30,(224,224,3))
-x0= np.floor(x0)
-for i in range(np.shape(x0)[0]):
-    for j in range(np.shape(x0)[1]):
-        for k in range(np.shape(x0)[2]):
-            if x0[i,j,k]>255.:
-                x0[i,j,k]=255.
-            elif x0[i,j,k]<0.:
-                x0[i,j,k]=0.
-x0_tensor = image.img_to_array(x0)
-x0_tensor = np.expand_dims(x0_tensor, axis=0)
-ipt_act = active_func(x0_tensor)
-
-#%%
-
+#Backend versions:
 def gram_Matrix_backend(activation):
     act=activation[0]
     s = act.shape
@@ -110,7 +81,6 @@ def get_Tloss_backend(activ2,layers_list):
         activ1= model.layers[idx].output
         width = np.shape(activ2[0][idx])[1]
         depth = np.shape(activ2[0][idx])[3]
-        print("step",num,"/",len(layers_list))
         num+=1
         loss += (1/(4*(depth*(width**2))**2))*K.sum((gram_Matrix_backend(activ1)[:,:]-gram_Matrix_backend(activ2[0][idx])[:,:])**2)#we assume that width=height
     return loss
@@ -118,52 +88,108 @@ def get_Tloss_backend(activ2,layers_list):
 def main_loss_backend(src_activ,layers):  
     return get_Tloss_backend(src_activ,layers)
 
-#def fun(alpha):#alpha is a 1D array of shape = (1,)
-#    act1 = active_func(x0_tensor-alpha[0]*iterate(x0_tensor))
-#    return texture_loss(layers_list,act1,src_act)
-
-#%%
-print("Start gradient computation")
-
-deriv=K.gradients(main_loss_backend(src_act,layers_list),model.input)[0]
-
-iterate = K.function([model.input], deriv)
-
-print(np.shape(iterate(x0_tensor)))
-print(np.shape(x0_tensor))
-
-#result=scipy.optimize.minimize(fun,np.array([0]),method='L-BFGS-B')#too long
-#%%
-print("Start gradient descent")
-
-u0=x0_tensor
-u=u0
-alpha=1e-2
-N=70
-Loss_list=[]
-for i in range(N):
-    print('step',i+1,'/',N)
-    u = u - alpha*iterate(u)*255/np.max(iterate(u))
-    u = (u-np.min(u))*255/(np.max(u)-np.min(u)) #streching d'histogramme
-    Loss_list.append(np.log(texture_loss(layers_list,active_func(u),src_act)))
+#save image 'u' to the declared path
+def save(path, u, mode=3):
+    if (mode==1)or(mode==3):
+        temp = (u-np.min(u))*254/(np.max(u)-np.min(u))
+        plt.imsave(path+'.jpg',temp[0]/255)
+    if (mode==2)or(mode==3):
+        tempb=u
+        tempb[u>255]=255
+        tempb[u<0]=0
+        plt.imsave(path+'b.jpg',tempb[0]/255)
     
-print('max=',np.max(u[0]))
-print('min=',np.min(u[0]))
-u0 = (u0-np.min(u0))*255/(np.max(u0)-np.min(u0))
-u = (u-np.min(u))*255/(np.max(u)-np.min(u))    
-plt.imshow((u0)[0]/255) 
-plt.figure()
-plt.imshow(u[0]/255)
-plt.figure()
-plt.imshow(src_img_tensor[0]/255)
-plt.figure()
-plt.plot(Loss_list)
+#add 'blur_shape' blur to an image    
+def blur_tensor(img_tensor,width,height,blur_shape):
+    rouge = scipy.signal.convolve(img_tensor[0,:,:,0],1/9*np.ones(blur_shape,dtype=int),mode='same')
+    vert = scipy.signal.convolve(img_tensor[0,:,:,1],1/9*np.ones(blur_shape,dtype=int),mode='same')
+    bleu = scipy.signal.convolve(img_tensor[0,:,:,2],1/9*np.ones(blur_shape,dtype=int),mode='same')
+    truc= np.zeros((width,height,3))
+    truc[:,:,0]=rouge
+    truc[:,:,1]=vert
+    truc[:,:,2]=bleu
+    return np.array([truc])
 
+#load an image and convert it into a tensor
+def load_tensor(img_path):
+    new_img = image.load_img(img_path, target_size=(224, 224),interpolation="bicubic")
+    new_tensor = image.img_to_array(new_img)                    # (height, width, channels)
+    return np.expand_dims(new_tensor, axis=0)
 
+#create white gaussian noise
+def create_noise(moy,sigma,shape):
+    x0 = np.random.normal(moy,sigma,shape)
+    x0= np.floor(x0)
+    for i in range(np.shape(x0)[0]):
+        for j in range(np.shape(x0)[1]):
+            for k in range(np.shape(x0)[2]):
+                if x0[i,j,k]>255.:
+                    x0[i,j,k]=255.
+                elif x0[i,j,k]<0.:
+                    x0[i,j,k]=0.
+    x0_tensor = image.img_to_array(x0)
+    x0_tensor = np.expand_dims(x0_tensor, axis=0)
+    return x0_tensor
 
+def create_grad_list(layer,active_func,src_tensor):
+    src_act=active_func(src_tensor)
+    output=[]
+    for lyr in layer:
+        output.append(K.function([model.input], K.gradients(main_loss_backend(src_act,lyr),model.input)[0]))
+    return output
 
+def grad_descent(u0,src_tensor,active_func,alpha_list,layer_list,iteration_list,plot_loss=False,plot_img=False,change_alpha=False,save_mode=0):
+    u=u0
+    src_act = active_func(src_tensor)
+    loss_list=[]
+    function_list=create_grad_list(layer_list,active_func,src_tensor)
+    if ((len(alpha_list)!=len(layer_list))or(len(alpha_list)!=len(iteration_list))or(len(alpha_list)!=len(function_list))):
+        raise Exception('Error: alpha_list, layer_list, iteration_list and function_list must have the same length.')
+    if ((change_alpha) and (not(plot_loss))):
+        raise Exception("Error: change_alpha can't be used without plot_loss (for the moment)")
+    if plot_img:
+        plt.figure()
+        plt.imshow(u0[0]/255)
+        plt.figure()
+        plt.imshow(src_tensor[0]/255)
+    for idx,layer in enumerate(layer_list):
+        print('layer n°',idx+1,'/',len(layer_list))
+        alpha=alpha_list[idx]
+        func=function_list[idx]
+        for i in range(iteration_list[idx]):
+            if (i%10==0):
+                print(".",end= " ")
+            u = u - alpha*func(u)*255/np.max(func(u))
+            if plot_loss:
+                loss_list.append(np.log(texture_loss(layer,active_func(u),src_act)))
+            if change_alpha and (i>2):
+                if (loss_list[-1]>loss_list[-2]):
+                    alpha=alpha/2
+        if plot_img:
+            plt.figure()
+            plt.imshow(u[0]/255)
+        save('layer'+str(idx),u,save_mode)
+    if plot_loss:
+        plt.figure()
+        plt.plot(loss_list)
+        plt.title('Loss for learning rate='+str(alpha_list)+';layers ='+str(layer_list)+';iterations='+str(iteration_list)+';change_alpha='+str(change_alpha))
+        plt.show()
+    return u[0]
 
+#%% Define the source texture and white noise
+src_img_tensor= load_tensor("texture3.jpg")
+x0_tensor = create_noise(127,30,(224,224,3))
+ 
+outputs = [layer.output for layer in model.layers]
+active_func = K.function([model.input], [outputs])
+src_act = active_func(src_img_tensor)
 
+print("Start gradient descent")     
 
+alphas=[1e-2,1e-2,1e-2,1e-2,1e-2]
+iterations=[2000,400,200,200,200]
+u0=blur_tensor(x0_tensor,224,224,(3,3))
+
+result=grad_descent(u0,src_img_tensor,active_func,alphas,layer,iterations,True,True,True,save_mode=2)    
 
 
